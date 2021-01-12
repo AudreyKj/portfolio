3cf0844f38840ff626a6464e0311958ce76af8ae
AUDREY KADJAR <audreykadjar@gmail.com>
2021-01-12 08:04:05 +0100

    save edits to temp

--- src/temp.js
+++ src/temp.js
@@ -3,35 +3,36 @@ import React from "react";
 function Temp() {
   return (
     <div className="temp">
-      <h1>
+      <h1 className='temp'>
         I'm currently updating my portfolio - please come back soon!
       </h1>
-      <p className="about-text">
-        In the meantime, contact me on: <br />
-        <a
-          className="links-about"
-          href="https://github.com/AudreyKj"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GitHub
+
+      <a className="links-about" href="mailto:audreykadjar@gmail.com">
+        Email: audreykadjar@gmail.com
         </a>
-        <br />
-        <a
-          className="links-about"
-          href="https://www.linkedin.com/in/audreykadjar/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          LinkedIn
-          <br />
-        </a>
-        <a className="links-about" href="mailto:audreykadjar@gmail.com">
-          audreykadjar@gmail.com
-        </a>
-      </p>
+
     </div>
   );
 }
 
 export default Temp;
+
+
+/* <a
+className="links-about"
+href="https://github.com/AudreyKj"
+target="_blank"
+rel="noopener noreferrer"
+>
+GitHub
+</a>
+- {' '}
+<a
+className="links-about"
+href="https://www.linkedin.com/in/audreykadjar/"
+target="_blank"
+rel="noopener noreferrer"
+>
+LinkedIn
+</a>
+- {' '} */
